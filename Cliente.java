07a0e93d7546b427ec3698cbc53fb1edffe1a652
Paulo Henrique Rezende de Oliveira <120255570+PHRezendeOliveira@users.noreply.github.com>
2024-08-07 08:33:18 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,33 @@
+public class Cliente {
+    private String id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Cliente(String id, String nome, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
